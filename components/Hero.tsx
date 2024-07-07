e50c2f006c2667cc24f1acf97b6aa07880ec5276
dealbisac <chand.dipendra19@gmail.com>
2024-07-07 12:09:36 +0200

    feat: Add Spotlight component to Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='pb-20 pt-36'>
+        <div>
+            <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20'/>
+        </div>
+    </div>
+    []
   )
 }
 
